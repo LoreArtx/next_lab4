43923a525ac1b4d33adbc5efba6bcba47dd5f3ce
Danylo Dziubchuk <vt211_ddr@student.ztu.edu.ua>
2024-06-02 01:05:41 +0300

    tests for main page

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,10 +1,23 @@
-/**
- * @jest-environment jsdom
- */
-import { render, screen } from "@testing-library/react";
-import Page from "./page";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Page, { metadata } from './page';
+import './components/__tests__/Button.test';
+import './components/__tests__/Input.test';
+import './components/__tests__/Label.test';
+import './components/__tests__/Navbar.test';
 
-it("App Router: Works with Server Components", () => {
-  render(<Page />);
-  expect(screen.getByRole("heading")).toHaveTextContent("App Router");
+describe('Page component', () => {
+  it('renders correctly', () => {
+    render(<Page />);
+  });
+
+  it('has correct metadata', () => {
+    expect(metadata).toEqual({ title: 'App Router' });
+  });
+
+  it('renders title correctly', () => {
+    render(<Page />);
+    const titleElement = screen.getByText('App Router');
+    expect(titleElement).toBeInTheDocument();
+  });
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Label from "./components/Label";
+import Navbar from "./components/Navbar";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return <>
+    <h1>App Router</h1>
+    <Navbar items={["Home", "About", "Contact Us"]} />
+    <Label htmlFor="type">Type here</Label>
+    <Input id="type"></Input>
+    <Button>Submit</Button>
+  </>
 }
